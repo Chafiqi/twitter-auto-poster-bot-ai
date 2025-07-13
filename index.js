4198ea2be7203d3801cf2e95423d234a337cfdca
Chafiqi <doctorallinout@gmail.com>
2025-07-13 19:44:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const { TwitterApi } = require("twitter-api-v2");
 const SECRETS = require("./SECRETS");
 
 const twitterClient = new TwitterApi({
-  const appKey = "90NMIhcE20J9UQOMyLHFSGBWB";
-  appSecret: SECRETS = ("./MrsBSJmni9XZGKfymZ8FOqfxUjkiyPxl8fSt9OSx3nyssC0pvi");
-  accessToken: SECRETS = ("./1595899449804525571-xLX0yV8fw9rqlHfk59J6PBI2sMo73n");
-  accessSecret: SECRETS = ("./6QCT94SCv06OO6piSFPPVdOExSfYLgcziPw6jEygq09Ft");
+  appKey: "90NMIhcE20J9UQOMyLHFSGBWB";
+  appSecret: "MrsBSJmni9XZGKfymZ8FOqfxUjkiyPxl8fSt9OSx3nyssC0pvi";
+  accessToken: "1595899449804525571-xLX0yV8fw9rqlHfk59J6PBI2sMo73n";
+  accessSecret: SECRETS: "6QCT94SCv06OO6piSFPPVdOExSfYLgcziPw6jEygq09Ft";
 });
 
 const generationConfig = {
