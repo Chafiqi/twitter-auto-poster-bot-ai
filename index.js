e95e8f6ec5545b55285b44b8f6207004ba54cb75
Chafiqi <doctorallinout@gmail.com>
2025-07-13 19:52:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { TwitterApi } = require("twitter-api-v2");
 const SECRETS = require("./SECRETS");
 
 const twitterClient = new TwitterApi({
-  appKey: "90NMIhcE20J9UQOMyLHFSGBWB";
+  appKey: "AIzaSyAOQHwEvNEeuHYNuvzWxAjzArewhxrryas";
   appSecret: "MrsBSJmni9XZGKfymZ8FOqfxUjkiyPxl8fSt9OSx3nyssC0pvi";
   accessToken: "1595899449804525571-xLX0yV8fw9rqlHfk59J6PBI2sMo73n";
   accessSecret: SECRETS: "6QCT94SCv06OO6piSFPPVdOExSfYLgcziPw6jEygq09Ft";
